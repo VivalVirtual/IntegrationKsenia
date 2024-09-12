db1ada7b72026e622abbdc97a9bcdc556eeded97
Vivaldi_Virtual-logic <camilla.vivaldi@virtual-logic.it>
2024-09-12 15:42:18 +0200

    Update websocket_super_user.py

--- custom_components/KseniaIntegration/websocket_super_user.py
+++ custom_components/KseniaIntegration/websocket_super_user.py
@@ -8,6 +8,7 @@ import logging
 import ssl
 from .crc import addCRC
 import json
+from .websocket_client import SimpleAlarmWebSocketClient
 _LOGGER = logging.getLogger(__name__)
 message_queue = Queue()
 
@@ -15,10 +16,11 @@ message_queue = Queue()
 class WebsocketSuperUser:
     """Class to manage WebSocket connection to the alarm system."""
 
-    def __init__(self, uri, macAddr, pinSuper, coordinator: AlarmDataCoordinator):
+    def __init__(self, uri, macAddr, pinSuper, coordinator: AlarmDataCoordinator, websocket: SimpleAlarmWebSocketClient):
         """Initialize the WebSocket client."""
         self._uri = uri
         self._websocket = None
+        self.userWebsocket = websocket
         self._connected = False
         self._mac = macAddr
         self._pinSuper = pinSuper
@@ -34,12 +36,15 @@ class WebsocketSuperUser:
             try:
                 await self._websocket.send(message)
 
-                # _LOGGER.info(f" \n Sent message: {message}  \n ")
+                _LOGGER.info(f" \n Sent message: {message}  \n ")
                 return message
             except (websockets.ConnectionClosedError, websockets.ConnectionClosedOK, OSError) as e:
                 _LOGGER.error(f"Connection lost: {e}")
+                self._running = False
                 self._connected = False
+
                 await self.connectSuperUser()
+                await self.userWebsocket.connect()
 
     async def receive(self):
         """Receive a message from the WebSocket server."""
@@ -52,35 +57,43 @@ class WebsocketSuperUser:
                 except (websockets.ConnectionClosedError, websockets.ConnectionClosedOK, OSError) as e:
                     _LOGGER.error(f"Connection lost: {e}")
                     self._connected = False
-                    await self.connectSuperUser()
-        return None
+                    self._running = False
+                    return None
 
     async def connectSuperUser(self):
         """Connect to the WebSocket server."""
-        try:
-            sslcontext = ssl.create_default_context()
-            sslcontext.options |= ssl.OP_LEGACY_SERVER_CONNECT
-            sslcontext.check_hostname = False
-            sslcontext.verify_mode = ssl.CERT_NONE
-            self._websocket = await websockets.connect(
-                self._uri, ssl=sslcontext, subprotocols=["KS_WSOCK"],  ping_interval=20, ping_timeout=10)
-            self._connected = True
-            _LOGGER.info(
-                f"Connected super user to WebSocket server at {self._uri}")
-        except Exception as e:
-            _LOGGER.error(f"Failed to connect to WebSocket server: {e}")
-            self._connected = False
+        response = None
+        while not self._connected:
+            try:
+                sslcontext = ssl.create_default_context()
+                sslcontext.options |= ssl.OP_LEGACY_SERVER_CONNECT
+                sslcontext.check_hostname = False
+                sslcontext.verify_mode = ssl.CERT_NONE
+                self._websocket = await websockets.connect(
+                    self._uri, ssl=sslcontext, subprotocols=["KS_WSOCK"],  ping_interval=20, ping_timeout=10)
+                self._connected = True
+                _LOGGER.info(
+                    f"Connected super user to WebSocket server at {self._uri}")
+                print("-------- connessione super user ripresa ")
+            except Exception as e:
+                _LOGGER.error(f"Failed to connect to WebSocket server: {e}")
+                print("-------- connessione persa ")
+                self._connected = False
+                time.sleep(30)
+        response = None
+        while response == None:
+            # Fa il login User
+            try:
+                await self.send(addCRC('{"SENDER":"012345678901", "RECEIVER":"' + self._mac +
+                                       '", "CMD":"LOGIN", "ID": "65535", "PAYLOAD_TYPE":"IP_SUPERV", "PAYLOAD":{ "PIN": "' + self._pinSuper + '"}, "TIMESTAMP":"' + str(int(time.time())) + '", "CRC_16":"0x0000"}'))
+                response = await asyncio.wait_for(self.receive(), timeout=60)
+                if response:
+                    data = json.loads(response)
+                    self._id = data['PAYLOAD']['ID_LOGIN']
+                    self._reciver = data['RECEIVER']
 
-        # Fa il login User
-        try:
-            await self.send(addCRC('{"SENDER":"012345678901", "RECEIVER":"' + self._mac +
-                                   '", "CMD":"LOGIN", "ID": "65535", "PAYLOAD_TYPE":"IP_SUPERV", "PAYLOAD":{ "PIN": "' + self._pinSuper + '"}, "TIMESTAMP":"' + str(int(time.time())) + '", "CRC_16":"0x0000"}'))
-            response = await self.receive()
-            data = json.loads(response)
-            self._id = data['PAYLOAD']['ID_LOGIN']
-            self._reciver = data['RECEIVER']
-        except Exception as e:
-            _LOGGER.error(f"Impossibile fare il login: {e}")
+            except Exception as e:
+                _LOGGER.error(f"Impossibile fare il login: {e}")
 
         await self.send(addCRC('{"SENDER":"012345678901", "RECEIVER":"' + self._mac + '", "CMD":"REALTIME","ID":"2","PAYLOAD_TYPE":"REGISTER","PAYLOAD":{ "ID_LOGIN": "' + str(self._id) + '" ,  "TYPES":["STATUS_PARTITIONS", "STATUS_ZONES", "ZONES"] },"TIMESTAMP" : "' + str(int(time.time())) + '","CRC_16":""}'))
         response = await self.receive()
@@ -89,15 +102,20 @@ class WebsocketSuperUser:
         asyncio.create_task(self.listen_for_messages())
 
     async def listen_for_messages(self):
+        message = ""
         """Listen for incoming messages in a loop."""
-        while self._running:
+        while self._running and message is not None:
+            print("running -- " + str(self._running))
             message = await self.receive()
+            print("stop" + str(self._running))
             if message:
                 await self.coordinator._async_update_data_realtime(message, False)
+
+        print("reconecting")
+        await self.connectSuperUser()
+        await self.userWebsocket.connect()
 
     def process_message(self, message):
         """Process the received WebSocket message."""
         # Logica per elaborare il messaggio
         _LOGGER.info(f"Processing message: {message}")
-
-

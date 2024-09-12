2af92cd0e0ffe72ae35cf7a94c3d5b739c8b2ffc
Vivaldi_Virtual-logic <camilla.vivaldi@virtual-logic.it>
2024-09-12 15:42:26 +0200

    Update __init__.py

--- custom_components/KseniaIntegration/__init__.py
+++ custom_components/KseniaIntegration/__init__.py
@@ -23,10 +23,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     coordinator = AlarmDataCoordinator(hass, websocket_client)
     websocket_super_user = WebsocketSuperUser(
-        websocket_uri, entry.data.get("macAddr"), entry.data.get('pinSuper'), coordinator)
+        websocket_uri, entry.data.get("macAddr"), entry.data.get('pinSuper'), coordinator, websocket_client)
 
     await websocket_super_user.connectSuperUser()
-    websocket_client.connectWebsocketSuperUser(websocket_super_user)
+
     # Crea entità, servizi, ecc.
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(

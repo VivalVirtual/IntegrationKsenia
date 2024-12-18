ed58d4a992706dfa92899ed0de94a9ea36934da3
Vivaldi_Virtual-logic <camilla.vivaldi@virtual-logic.it>
2024-12-18 12:35:22 +0100

    Update coordinator.py

--- custom_components/KseniaIntegration/coordinator.py
+++ custom_components/KseniaIntegration/coordinator.py
@@ -1,4 +1,4 @@
-from homeassistant.const import STATE_ALARM_ARMED_AWAY, STATE_ALARM_DISARMED, STATE_ALARM_TRIGGERED, STATE_ALARM_ARMED_CUSTOM_BYPASS, STATE_ON
+from homeassistant.components.alarm_control_panel import AlarmControlPanelState
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.core import HomeAssistant
 from .websocket_client import SimpleAlarmWebSocketClient
@@ -7,6 +7,7 @@ from .const import *
 import logging
 import json
 import async_timeout
+
 SCAN_INTERVAL = timedelta(seconds=10)
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,11 +24,10 @@ class AlarmDataCoordinator(DataUpdateCoordinator):
         scenarios = client._scenarios
         partizioni = client._partizioni
         stato_partizioni = client._partizionistato
-        self._stato_allarme = STATE_ALARM_ARMED_AWAY
+        self._stato_allarme = AlarmControlPanelState.ARMED_AWAY
         self.TMP_ZONE = self.format_zone(zone, stato_zone, True)
         self.TMP_SCENARIOS = self.format_scenari(scenarios)
-        self.TMP_PARTIZIONI = self.format_partizioni(
-            partizioni, stato_partizioni, True)
+        self.TMP_PARTIZIONI = self.format_partizioni(partizioni, stato_partizioni, True)
         self.data = {
             DATA_PARTITIONS: self.TMP_PARTIZIONI,
             DATA_ZONES: self.TMP_ZONE,
@@ -37,7 +37,9 @@ class AlarmDataCoordinator(DataUpdateCoordinator):
     def format_zone(self, zone: any, stato_zone: any, firsMessage: bool):
         if firsMessage:
             TMP_ZONE = {}
-            for value, value_stato in zip(zone['PAYLOAD']['ZONES'], stato_zone['PAYLOAD']['STATUS_ZONES']):
+            for value, value_stato in zip(
+                zone["PAYLOAD"]["ZONES"], stato_zone["PAYLOAD"]["STATUS_ZONES"]
+            ):
                 description = value["DES"]
                 if value_stato["BYP"] == "NO":
                     status = ZONE_BYPASS_OFF
@@ -48,12 +50,17 @@ class AlarmDataCoordinator(DataUpdateCoordinator):
                 elif value_stato["STA"] == "A":
                     allarme = ZONE_STATUS_ALARM
                 key = value["ID"]
-                TMP_ZONE[key] = {"description": description,
-                                 "status": status, "allarme": allarme}
+                TMP_ZONE[key] = {
+                    "description": description,
+                    "status": status,
+                    "allarme": allarme,
+                }
             return TMP_ZONE
         else:
-
-            for value, value_stato in zip(zone['PAYLOAD']['ZONES'], stato_zone['PAYLOAD'][self.client._reciver]['STATUS_ZONES']):
+            for value, value_stato in zip(
+                zone["PAYLOAD"]["ZONES"],
+                stato_zone["PAYLOAD"][self.client._reciver]["STATUS_ZONES"],
+            ):
                 description = value["DES"]
                 if value_stato["BYP"] == "NO":
                     status = ZONE_BYPASS_OFF
@@ -65,13 +72,16 @@ class AlarmDataCoordinator(DataUpdateCoordinator):
                 elif value_stato["STA"] == "A":
                     allarme = ZONE_STATUS_ALARM
                 key = value_stato["ID"]
-                self.TMP_ZONE[key] = {"description": description,
-                                      "status": status, "allarme": allarme}
+                self.TMP_ZONE[key] = {
+                    "description": description,
+                    "status": status,
+                    "allarme": allarme,
+                }
 
     def format_scenari(self, scenarios: any):
         TMP_SCENARIOS = {}
 
-        for value in scenarios['PAYLOAD']['SCENARIOS']:
+        for value in scenarios["PAYLOAD"]["SCENARIOS"]:
             description = value["DES"]
             if value["CAT"] == "ARM":
                 status = SCENE_ARM
@@ -84,8 +94,9 @@ class AlarmDataCoordinator(DataUpdateCoordinator):
 
         return TMP_SCENARIOS
 
-    def format_partizioni(self, partizioni: any, stato_partizioni: any, firsMessage: bool):
-
+    def format_partizioni(
+        self, partizioni: any, stato_partizioni: any, firsMessage: bool
+    ):
         zone_bypassate = False
         zone_non_bypassate = False
         """if (update):
@@ -93,68 +104,84 @@ class AlarmDataCoordinator(DataUpdateCoordinator):
         else: """
         if firsMessage:
             TMP_PARTITIONS = {}
-            for value, value_stato in zip(partizioni['PAYLOAD']['PARTITIONS'], stato_partizioni['PAYLOAD']['STATUS_PARTITIONS']):
+            for value, value_stato in zip(
+                partizioni["PAYLOAD"]["PARTITIONS"],
+                stato_partizioni["PAYLOAD"]["STATUS_PARTITIONS"],
+            ):
                 description = value["DES"]
                 if value_stato["ARM"] == "D":
-                    status = STATE_ALARM_DISARMED
-                elif (value_stato["ARM"] == "IA"):
-                    status = STATE_ALARM_ARMED_HOME
+                    status = AlarmControlPanelState.DISARMED
+                elif value_stato["ARM"] == "IA":
+                    status = AlarmControlPanelState.ARMED_HOME
                 else:
-                    status = STATE_ALARM_ARMED_AWAY
+                    status = AlarmControlPanelState.ARMED_AWAY
                 key = value["ID"]
-                TMP_PARTITIONS[key] = {
-                    "description": description, "status": status}
+                TMP_PARTITIONS[key] = {"description": description, "status": status}
             return TMP_PARTITIONS
         else:
-
-            for value, value_stato in zip(partizioni['PAYLOAD']['PARTITIONS'], stato_partizioni['PAYLOAD'][self.client._reciver]['STATUS_PARTITIONS']):
+            for value, value_stato in zip(
+                partizioni["PAYLOAD"]["PARTITIONS"],
+                stato_partizioni["PAYLOAD"][self.client._reciver]["STATUS_PARTITIONS"],
+            ):
                 description = value["DES"]
                 if value_stato["ARM"] == "D":
-                    status = STATE_ALARM_DISARMED
-                elif (value_stato["ARM"] == "IA"):
-                    status = STATE_ALARM_ARMED_HOME
+                    status = AlarmControlPanelState.DISARMED
+                elif value_stato["ARM"] == "IA":
+                    status = AlarmControlPanelState.ARMED_HOME
                 else:
-                    status = STATE_ALARM_ARMED_AWAY
+                    status = AlarmControlPanelState.ARMED_AWAY
                 key = value_stato["ID"]
                 self.TMP_PARTIZIONI[key] = {
-                    "description": description, "status": status}
+                    "description": description,
+                    "status": status,
+                }
 
-    async def _async_update_data_realtime(self, realTimeDati, firstMessage: bool) -> dict:
+    async def _async_update_data_realtime(
+        self, realTimeDati, firstMessage: bool
+    ) -> dict:
         if firstMessage:
-
             self.TMP_PARTIZIONI = self.format_partizioni(
-                self.client._partizioni, json.loads(realTimeDati), True)
+                self.client._partizioni, json.loads(realTimeDati), True
+            )
             self.TMP_ZONE = self.format_zone(
-                self.client._zone, json.loads(realTimeDati), True)
-            self.data = {DATA_PARTITIONS: self.TMP_PARTIZIONI,
-                         DATA_ZONES: self.TMP_ZONE, DATA_SCENARIOS: self.TMP_SCENARIOS}
+                self.client._zone, json.loads(realTimeDati), True
+            )
+            self.data = {
+                DATA_PARTITIONS: self.TMP_PARTIZIONI,
+                DATA_ZONES: self.TMP_ZONE,
+                DATA_SCENARIOS: self.TMP_SCENARIOS,
+            }
 
         else:
-            if 'STATUS_PARTITIONS' in realTimeDati:
+            if "STATUS_PARTITIONS" in realTimeDati:
                 try:
-                    value = self.client._partizioni['PAYLOAD']['PARTITIONS']
+                    value = self.client._partizioni["PAYLOAD"]["PARTITIONS"]
                 except KeyError:
                     self.client.lettura_zone
                 self.format_partizioni(
-                    self.client._partizioni, json.loads(realTimeDati), False)
-                self.data = {DATA_PARTITIONS: self.TMP_PARTIZIONI,
-                             DATA_ZONES: self.TMP_ZONE, DATA_SCENARIOS: self.TMP_SCENARIOS}
-            if 'STATUS_ZONES' in realTimeDati:
-
+                    self.client._partizioni, json.loads(realTimeDati), False
+                )
+                self.data = {
+                    DATA_PARTITIONS: self.TMP_PARTIZIONI,
+                    DATA_ZONES: self.TMP_ZONE,
+                    DATA_SCENARIOS: self.TMP_SCENARIOS,
+                }
+            if "STATUS_ZONES" in realTimeDati:
                 try:
-                    value = self.client._zone['PAYLOAD']['ZONES']
+                    value = self.client._zone["PAYLOAD"]["ZONES"]
 
                 except KeyError:
                     self.client.lettura_zone
 
-                self.format_zone(
-                    self.client._zone, json.loads(realTimeDati), False)
+                self.format_zone(self.client._zone, json.loads(realTimeDati), False)
 
-                self.data = {DATA_PARTITIONS: self.TMP_PARTIZIONI,
-                             DATA_ZONES: self.TMP_ZONE, DATA_SCENARIOS: self.TMP_SCENARIOS}
+                self.data = {
+                    DATA_PARTITIONS: self.TMP_PARTIZIONI,
+                    DATA_ZONES: self.TMP_ZONE,
+                    DATA_SCENARIOS: self.TMP_SCENARIOS,
+                }
         self.async_set_updated_data(await self._async_update_data())
 
     async def _async_update_data(self) -> dict:
-
         async with async_timeout.timeout(DEFAULT_TIMEOUT):
             return self.data
